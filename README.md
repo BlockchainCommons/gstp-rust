cd75cfdb7dd62962b52ee19c895d95aa09c18876
Wolf McNally <wolf@wolfmcnally.com>
2024-12-02 18:50:14 -0800

    Prep for release.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 ## Introduction
 
-The Gordian Sealed Transaction Protocol (GSTP) is a secure, transport-agnostic communication method enabling encrypted and signed data exchange between multiple parties. Built upon the Gordian Envelope specification, GSTP supports various transport mediums—including HTTP, raw TCP/IP, air-gapped protocols using QR codes, and NFC cards—by implementing its own encryption and signing protocols. A key feature of GSTP is Encrypted State Continuations (ESC), which embed encrypted state data directly into messages, eliminating the need for local state storage and enhancing security for devices with limited storage or requiring distributed state management. It facilitates both client-server and peer-to-peer architectures, ensuring secure and flexible communication across diverse platforms.
+Gordian Sealed Transaction Protocol (GSTP) is a secure, transport-agnostic communication method enabling encrypted and signed data exchange between multiple parties. Built upon the Gordian Envelope specification, GSTP supports various transport mediums—including HTTP, raw TCP/IP, air-gapped protocols using QR codes, and NFC cards—by implementing its own encryption and signing protocols. A key feature of GSTP is Encrypted State Continuations (ESC), which embed encrypted state data directly into messages, eliminating the need for local state storage and enhancing security for devices with limited storage or requiring distributed state management. It facilitates both client-server and peer-to-peer architectures, ensuring secure and flexible communication across diverse platforms.
 
 ## Getting Started
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+#![doc(html_root_url = "https://docs.rs/gstp/0.1.0")]
+#![warn(rust_2018_idioms)]
+
+//! # Introduction
+//!
+//! Gordian Sealed Transaction Protocol (GSTP) is a secure, transport-agnostic
+//! communication method enabling encrypted and signed data exchange between
+//! multiple parties. Built upon the Gordian Envelope specification, GSTP
+//! supports various transport mediums—including HTTP, raw TCP/IP, air-gapped
+//! protocols using QR codes, and NFC cards—by implementing its own encryption
+//! and signing protocols. A key feature of GSTP is Encrypted State
+//! Continuations (ESC), which embed encrypted state data directly into
+//! messages, eliminating the need for local state storage and enhancing
+//! security for devices with limited storage or requiring distributed state
+//! management. It facilitates both client-server and peer-to-peer
+//! architectures, ensuring secure and flexible communication across diverse
+//! platforms.
+//!
+//! # Getting Started
+//!
+//! ```toml
+//! [dependencies]
+//! gstp = "0.1.0"
+//! ```
+//!
+//! # Examples
+//!
+//! See the unit tests in the source code for examples of how to use this
+//! library.
+
 mod continuation;
 pub use continuation::Continuation;
 mod sealed_request;
@@ -447,5 +477,15 @@ mod tests {
         assert_eq!(parsed_event.content(), "test");
         assert_eq!(parsed_event.note(), "This is a test");
         assert_eq!(parsed_event.date(), Some(&now));
+    }
+
+    #[test]
+    fn test_readme_deps() {
+        version_sync::assert_markdown_deps_updated!("README.md");
+    }
+
+    #[test]
+    fn test_html_root_url() {
+        version_sync::assert_html_root_url_updated!("src/lib.rs");
     }
 }
